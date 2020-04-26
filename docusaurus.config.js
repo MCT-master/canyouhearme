253771b92e7837c13b30098ac68acc6ef7751cb1
Jackson Goode <54308792+jacksonmgoode@users.noreply.github.com>
2020-04-27 01:11:21 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,92 @@
+module.exports = {
+  title: 'Can You Hear Me?',
+  tagline: 'For all confusing audio-video menagerie',
+  url: 'https://jacksonmgoode.github.io',
+  baseUrl: '/mct-av-wiki',
+  favicon: 'img/favicon.ico',
+  organizationName: 'jacksonmgoode', // Usually your GitHub org/user name.
+  projectName: 'mct-av-wiki', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'CYHM',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {
+          to: 'docs/doc1',
+          activeBasePath: 'docs',
+          label: 'Wiki',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Guides', position: 'left'},
+        {
+          href: 'https://github.com/MCT-master',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/rzcKcxy',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/MCT-master',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/MCT_master',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} MCT A/V Wiki`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
